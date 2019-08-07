0580a2a6e20995248619d0c598d5eecbe448cc25
John Jacobs <john.a.jacobs1973@gmail.com>
2019-08-07 15:54:01 -0400

    added img url field

--- cookbook_app/models.py
+++ cookbook_app/models.py
@@ -2,8 +2,9 @@ from django.db import models
 import datetime
 
 class Cookbook(models.Model):
-    title = models.CharField(blank=True,max_length=255)
-    editor = models.CharField(blank=True,max_length=255)
+    title = models.CharField(blank=True, max_length=255)
+    editor = models.CharField(blank=True, max_length=255)
+    img_url = models.CharField(blank=True, max_length=255)
     year_published = models.DateField(default=datetime.date.today)
 
     def __str__(self):
@@ -15,6 +16,7 @@ class Recipe(models.Model):
     name = models.CharField(blank=True, max_length=255)
     chef = models.CharField(blank=True, max_length=255)
     directions = models.TextField(blank=True)
+    img_url = models.CharField(blank=True, max_length=255)
     cookbook = models.ForeignKey(Cookbook, on_delete=models.CASCADE, related_name='recipes')
 
     def __str__(self):
@@ -24,6 +26,7 @@ class Recipe(models.Model):
 class Ingredient(models.Model):
     name = models.CharField(blank=True, max_length=255)
     retailer = models.CharField(blank=True, max_length=255)
+    img_url = models.CharField(blank=True, max_length=255)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
--- cookbook_app/serializers.py
+++ cookbook_app/serializers.py
@@ -5,16 +5,16 @@ from .models import Cookbook, Recipe, Ingredient
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ('id', 'name', 'retailer')
+        fields = ('id', 'name', 'retailer', 'img_url')
 
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientSerializer(many=True, read_only=True)
     class Meta:
         model = Recipe
-        fields = ('id', 'name', 'chef', 'directions', 'ingredients')
+        fields = ('id', 'name', 'chef', 'directions', 'ingredients', 'img_url')
 
 class CookbookSerializer(serializers.ModelSerializer):
     recipes = RecipeSerializer(many=True, read_only=True)
     class Meta:
         model = Cookbook
-        fields = ('id',  'title', 'editor', 'year_published', 'recipes')
+        fields = ('id',  'title', 'editor', 'year_published', 'recipes', 'img_url')
