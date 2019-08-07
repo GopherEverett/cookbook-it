160b1ddbb63ba44b044bfac60ec7a0a16cac4fd1
John Jacobs <john.a.jacobs1973@gmail.com>
2019-08-07 09:17:27 -0400

    dumb misspelling

--- cookbook_app/models.py
+++ cookbook_app/models.py
@@ -18,7 +18,7 @@ class Recipe(models.Model):
     
 
 class Ingredient(models.Model):
-    name = models.CharField(max_lemgth=255)
+    name = models.CharField(max_length=255)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
 
     def __str__(self):
