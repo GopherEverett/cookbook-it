5f28df4fbd0c989ca0b787bf38714f5219022564
John Jacobs <john.a.jacobs1973@gmail.com>
2019-08-08 16:42:55 -0400

    bootstrap cdn

--- client/src/components/Cookbook.jsx
+++ client/src/components/Cookbook.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
 export default class Cookbook extends Component {
     state = {
         cookbook: {
             recipes: []
-        }
+        },
+        reDir: false
     }
     componentDidMount() {
         const cookbookId = this.props.match.params.id
         this.fetchCookbook(cookbookId)
+    }
+
+    handleDelete = async () => {
+        try {
+            await axios.delete(`/api/v1/cookbooks/${this.props.match.params.id}/`)
+            this.setState({
+                reDir: true
+            })
+        }
+        catch (err) {
+            console.log(err)
+        }
     }
 
     fetchCookbook = async (cookbookId) => {
@@ -26,11 +39,15 @@ export default class Cookbook extends Component {
         }
     }
     render() {
+        if (this.state.reDir === true) {
+            return <Redirect to='/' />
+        }
         return (
             <div>
                 <img src={this.state.cookbook.img_url} alt="" />
                 <h1>{this.state.cookbook.title}</h1>
                 <h3>Editor: {this.state.cookbook.editor} - Published:  {this.state.cookbook.year_published}</h3>
+                <button onClick={this.handleDelete}>Delete</button>
                 <h3>Recipes: </h3>
                 {this.state.cookbook.recipes.map(recipe => (
                     <div key={recipe.id}>
